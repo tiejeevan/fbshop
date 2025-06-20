ef2c24c74c931b99e246c62b6934efa422b6c160
jack baby <tiejeevan@gmail.com>
2025-06-20 04:11:50 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- middleware.ts
+++
@@ -1,3 +0,0 @@
-// This file is intentionally left blank as it has been moved to src/middleware.ts
-// The build system should remove this file if it's no longer referenced.
-// If you see this file after the changes, it means it wasn't correctly removed by the build process.
--- src/app/signup/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { SignupForm } from '@/components/auth/SignupForm';
-
-export default function CustomerSignupPage() {
-  return <SignupForm loginPath="/login" />;
-}
